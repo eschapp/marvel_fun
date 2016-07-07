e203797d9fc3fc74cb9a713193ef0a00b906af2c
eschapp <eschapp@gmail.com>
2016-07-07 09:22:36 -0500

    marvel fun setup

---
+++ marvel.js
@@ -0,0 +1,31 @@
+console.log('hello there');
+
+function fetchJSON(url) {
+  return fetch(url).then(function(response) {
+    var contentType = response.headers.get("content-type");
+    if(contentType && contentType.indexOf("application/json") !== -1) {
+      return response.json();
+    } else {
+      console.log("Oops, we haven't got JSON!");
+    }
+  });
+}
+
+function marvelFactory(config) {
+  return function(path) {
+    var timestamp = new Date().getTime();
+    var hash = CryptoJS.MD5(timestamp + config.privateKey + config.publicKey).toString();
+    var url = config.hostname + '/v' + config.version + '/public' + path + '?apikey=' + config.publicKey + '&ts=' + timestamp + '&hash=' + hash;
+    console.log(url);
+
+    return fetchJSON(url);
+  }
+}
+
+// Get an instance of the marvel api
+var marvel = marvelFactory({
+  hostname: 'http://gateway.marvel.com',
+  publicKey: 'c5ebb123335240358c2744cde0f38b7e',
+  privateKey: 'cff3343714de139da0173348487355979eda8406',
+  version: '1'
+});
