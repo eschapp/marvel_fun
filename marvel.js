b74abdfe98496257974d6d4d471faca3d382fb76
eschapp <eschapp@gmail.com>
2016-07-08 11:28:42 -0500

    comic book work

--- marvel.js
+++ marvel.js
@@ -17,8 +17,7 @@ function marvelFactory(config) {
     var hash = md5.create();
     hash.update(timestamp + config.privateKey + config.publicKey);
     hash.hex();
-    // var hash = CryptoJS.MD5(timestamp + config.privateKey + config.publicKey).toString();
-    var url = config.hostname + '/v' + config.version + '/public' + path + '?apikey=' + config.publicKey + '&ts=' + timestamp + '&hash=' + hash;
+    var url = config.hosttitle + '/v' + config.version + '/public' + path + '?limit=100' + '&apikey=' + config.publicKey + '&ts=' + timestamp + '&hash=' + hash;
     console.log(url);
 
     return fetchJSON(url);
@@ -27,12 +26,74 @@ function marvelFactory(config) {
 
 // Get an instance of the marvel api
 var marvel = marvelFactory({
-  hostname: 'http://gateway.marvel.com',
+  hosttitle: 'http://gateway.marvel.com',
   publicKey: 'c5ebb123335240358c2744cde0f38b7e',
   privateKey: 'cff3343714de139da0173348487355979eda8406',
   version: '1'
 });
 
-marvel('/characters').then(function(json) {
+
+function $(selector) {
+  return document.querySelector(selector);
+}
+
+$.create = function(elementtitle) {
+  return document.createElement(elementtitle);
+}
+
+$.createText = function(text) {
+  return document.createTextNode(text);
+}
+
+$.setAttribute = function(el, attr, value) { // this
+  return el.setAttribute(attr, value);
+};
+
+$.appendChild = function(parentElement, childElement) {
+  return parentElement.appendChild(childElement);
+}
+
+function makeFilePath(path, extension) {
+  return path + '.' + extension;
+}
+
+function marvelImageFound(path) {
+  return (path !== 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg');
+}
+
+marvel('/comics').then(function(json) {
   console.log(json);
+  var container = $('comics');
+
+  var noImageArray = json.data.results.filter(function(comic){
+
+    var comicContainer = $.create('comic');
+
+    var imgPath = makeFilePath(comic.thumbnail.path, comic.thumbnail.extension);
+    var title = comic.title;
+
+    var img = $.create('img');
+    $.setAttribute(img, 'src', imgPath);
+
+    var titleTag = $.create('comic-title');
+
+    var titleTextNode = $.createText(title);
+    var titleLinkNode = $.create('a');
+
+    $.setAttribute(titleLinkNode, 'href', 'https://www.google.com/#q=' + encodeURIComponent(title));
+    $.appendChild(titleLinkNode, titleTextNode);
+
+    $.appendChild(titleTag, titleLinkNode);
+
+    $.appendChild(comicContainer, titleTag);
+    $.appendChild(comicContainer, img);
+
+    var imgFound = marvelImageFound(imgPath);
+    if(imgFound) {
+      $.appendChild(container, comicContainer);
+    }
+    return !imgFound;
+
+  });
+
 });
